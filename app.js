64fd5c189d1c1a2cc9f6fb5a7378d099d74ffca2
suhelkhanca <suhel.hata@gmail.com>
2024-04-03 15:43:50 +0530

    added showWINNER FXN

--- app.js
+++ app.js
@@ -50,3 +50,9 @@ const checkWinner = () => {
       }
     }
   };
+  
+  const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+  };
