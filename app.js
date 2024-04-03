25947acf14373e57de9e6ad4b18e3620cd6d4d5d
suhelkhanca <suhel.hata@gmail.com>
2024-04-03 15:41:06 +0530

    winner logic

--- app.js
+++ app.js
@@ -25,5 +25,13 @@ boxes.forEach((box)=> {
             turnO = true;
         }
         box.disabled = true;
+        
+        count++;
+    
+        let isWinner = checkWinner();
+    
+        if (count === 9 && !isWinner) {
+          gameDraw();
+        }
     });
 });
